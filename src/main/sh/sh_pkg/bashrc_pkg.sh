03a66071c2f8694f399511678bec48fc9f451ce7
Sean Champ <spchamp@users.noreply.github.com>
2019-09-21 18:12:21 -0700

    bashrc_pkg.sh: Update annotations

--- src/main/sh/sh_pkg/bashrc_pkg.sh
+++ src/main/sh/sh_pkg/bashrc_pkg.sh
@@ -7,11 +7,8 @@
 #        for shell processes (non-chroot) under pkgsrc.
 #
 # Remarks
-# - This shell script may not depend expressly on BASH. It's believed
-#   that the BASH built-in 'test' cmd may behave differently than some
-#   BSD 'test' cmd under '-z' and/or '-n' tests. As such, it may be
-#   advisable to use this shell script with BASH environments only.
-#   (not tested under any other SH)
+# - This shell script uses some BASH extensions, e.g BASH_SOURCE[0] and
+#   as such, may not function as desired under any other SH
 #
 
 
@@ -68,6 +65,8 @@ _path_add ${PKGSRC_PREFIX}/sbin:${PKGSRC_PREFIX}/bin
 
 ## - TBD: gtk-doc paths; ScrollKeeper/Rarian paths; ...
 
+## -- Manual Pages
+
 MANPATH=${PKGSRC_PREFIX}/man:${PKGSRC_PREFIX}/share/man:${HOST_PREFIX}/man:${HOST_PREFIX}/share/man:${SYS_PREFIX}/share/man
 
 for OPT in guile/2.2/man gnu/man; do
@@ -75,13 +74,12 @@ for OPT in guile/2.2/man gnu/man; do
 [ -e ${PKGSRC_PREFIX}/${OPT}/ ] && MANPATH=${PKGSRC_PREFIX}/${OPT}:${MANPATH}
 done
 
+# -- GNU Texinfo
+
 ## NB GNU coreutils, GNU binutils, GCC info, and Guile info documentation
-## should be (??) installed under the conventional PKGSRC infodir
+## typically would be installed under the conventional PKGSRC infodir
 
 INFOPATH=${PKGSRC_PREFIX}/info:${PKGSRC_PREFIX}/share/info:${HOST_PREFIX}/info:${HOST_PREFIX}/share/info:${SYS_PREFIX}/share/info
-## NB Editor/IDE suport - Emacs `Info-directory-list`
-##
-## NB lang/guile20 and lang/guile22 would conficlit in the Guile info docs
 
 ## TBD: set MAKESYSPATH (pkgsrc bootstrap-mk-files or sys mk-files @ host)
 ## sys mk-files as sys mk-files [BSD] ::
@@ -89,6 +87,7 @@ INFOPATH=${PKGSRC_PREFIX}/info:${PKGSRC_PREFIX}/share/info:${HOST_PREFIX}/info:$
 ## bootstrap-mk-files as sys mk-files [Other] ::
 # MAKESYSPATH=${PKGSRC_PREFIX}/share/mk
 
+## -- pkgconf
 
 PKG_CONFIG_LIBDIR=${PKGSRC_PREFIX}/lib/pkgconfig:${HOST_PREFIX}/lib/pkgconfig:${SYS_PREFIX}/lib/pkgconfig
 ## ^ NB PKG_CONFIG_LIBDIR will be set, during builds, in mk/tools/pkg-config.mk
@@ -99,8 +98,7 @@ PKG_CONFIG_LIBDIR=${PKGSRC_PREFIX}/lib/pkgconfig:${HOST_PREFIX}/lib/pkgconfig:${
 
 export PATH MANPATH INFOPATH PKG_CONFIG_LIBDIR  # MAKESYSPATH
 
-## CCACHE config-in-process-environment
-
+## -- CCACHE config-in-process-environment
 
 ## NB: Leaving CCACHE_PATH to a careful $PATH config
 ##     such that can be modified in the build eviroment
